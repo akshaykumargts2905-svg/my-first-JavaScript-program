d370c2a597d2f0697d8a341a358175b8ddce7e2e
Akshay Kumar <akshaykumargts2905@gmail.com>
2026-05-21 10:29:23 +0530

    divisible by using function

---
+++ function2.js
@@ -0,0 +1,13 @@
+function isdivisible(num1, num2) {
+  if (num1 % num2 == 0) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+}
+isdivisible(36, 6);
+isdivisible(0, 10);
+isdivisible(10, 15);
+isdivisible(25, 5);
+isdivisible(1, 1);
+isdivisible(4, 7);
